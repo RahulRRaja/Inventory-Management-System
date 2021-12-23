8ab95cfb1522ea48aff52b847e97c3bd0ed839a8
RahulRRaja <93483148+RahulRRaja@users.noreply.github.com>
2021-12-23 16:19:24 +0530

    Update ProductRestController.java

--- Inventory management system/Inventory-management-system/src/main/java/com/sample/Inventory/management/system/restcontroller/ProductRestController.java
+++ Inventory management system/Inventory-management-system/src/main/java/com/sample/Inventory/management/system/restcontroller/ProductRestController.java
@@ -14,31 +14,31 @@ public class ProductRestController {
     private ProductService productService;
 
     @RequestMapping(value = "/allProduct", method = RequestMethod.GET, headers = "Accept=application/json")
-    public List<Product> getAllProduct() {
+    public List<Product> getAllProduct() throws InternalServerErrorException {
         List<Product> listProduct = productService.getAllProducts();
         return listProduct;
     }
 
     @RequestMapping(value = "/getProductById/{id}", method = RequestMethod.GET, headers = "Accept=application/json")
-    public Product getProduct(@PathVariable("id") String id) {
+    public Product getProduct(@PathVariable("id") String id) throws InternalServerErrorException {
         Product product = productService.getProduct(Integer.parseInt(id));
         return product;
     }
 
     @RequestMapping(value = "/addProduct", method = RequestMethod.POST, headers = "Accept=application/json")
-    public Product saveProduct(@RequestBody Product p) {
+    public Product saveProduct(@RequestBody Product p) throws InternalServerErrorException {
         productService.createProduct(p);
         return p;
     }
 
     @RequestMapping(value = "/updateProduct", method = RequestMethod.PUT)
-    public Product updateProduct(@RequestBody Product p) {
+    public Product updateProduct(@RequestBody Product p) throws InternalServerErrorException {
         productService.editProduct(p);
         return p;
     }
 
     @RequestMapping(value = "/deleteProduct/{id}", method = RequestMethod.DELETE, headers = "Accept=application/json")
-    public Product deleteProduct(@PathVariable("id") String id) {
+    public Product deleteProduct(@PathVariable("id") String id) throws InternalServerErrorException {
         productService.deleteProduct(Integer.parseInt(id));
         Product p = new Product();
         return p;
