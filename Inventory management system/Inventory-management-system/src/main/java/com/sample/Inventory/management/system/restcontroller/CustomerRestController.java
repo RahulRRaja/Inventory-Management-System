ec826646b12bb1af4483aec84d962d3f1cd2fc0e
RahulRRaja <93483148+RahulRRaja@users.noreply.github.com>
2021-12-23 16:17:40 +0530

    Update CustomerRestController.java

--- Inventory management system/Inventory-management-system/src/main/java/com/sample/Inventory/management/system/restcontroller/CustomerRestController.java
+++ Inventory management system/Inventory-management-system/src/main/java/com/sample/Inventory/management/system/restcontroller/CustomerRestController.java
@@ -14,30 +14,30 @@ public class CustomerRestController {
     private CustomerService customerService;
 
     @RequestMapping(value = "/allCustomer", method = RequestMethod.GET, headers = "Accept=application/json")
-    public List<Customer> getAllCustomer() {
+    public List<Customer> getAllCustomer() throws InternalServerErrorException {
         return customerService.getAllCustomers();
     }
 
     @RequestMapping(value = "/getCustomerById/{id}", method = RequestMethod.GET, headers = "Accept=application/json")
-    public Customer getCustomer(@PathVariable("id") String id) {
+    public Customer getCustomer(@PathVariable("id") String id) throws InternalServerErrorException {
         Customer customer = customerService.getCustomer(Integer.parseInt(id));
         return customer;
     }
 
     @RequestMapping(value = "/addCustomer", method = RequestMethod.POST, headers = "Accept=application/json")
-    public Customer saveCustomer(@RequestBody Customer p) {
+    public Customer saveCustomer(@RequestBody Customer p) throws InternalServerErrorException {
         customerService.createCustomer(p);
         return p;
     }
 
     @RequestMapping(value = "/updateCustomer", method = RequestMethod.PUT)
-    public Customer updateCustomer(@RequestBody Customer p) {
+    public Customer updateCustomer(@RequestBody Customer p) throws InternalServerErrorException {
         customerService.editCustomer(p);
         return p;
     }
 
     @RequestMapping(value = "/deleteCustomer/{id}", method = RequestMethod.DELETE, headers = "Accept=application/json")
-    public Customer deleteCustomer(@PathVariable("id") String id) {
+    public Customer deleteCustomer(@PathVariable("id") String id) throws InternalServerErrorException {
         customerService.deleteCustomer(Integer.parseInt(id));
         Customer p = new Customer();
         return p;
